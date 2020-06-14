97a294ed1653d74700302f0bc459610f78023655
Deep Majumder <deep.majumder2019@gmail.com>
2020-06-14 19:37:40 +0530

    Added tailwind.css

---
+++ visualiser/src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Board extends React.Component {
+    render() {
+        return <h1>Hello</h1>
+    }
+}
+
+class Tile extends React.Component {
+    constructor() {
+
+    }
+
+    render() {
+
+    }
+}
+
+class App {
+    constructor() {
+
+    }
+
+    render() {
+
+    }
+}
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('board')
+)
